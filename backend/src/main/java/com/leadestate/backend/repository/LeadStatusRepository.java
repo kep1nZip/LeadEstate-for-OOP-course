f09bd3b49d5e2837f9e1910167aa6deda1ac65c4
FenFirasy <firasy720@gmail.com>
2026-06-16 21:11:34 +0700

    rapihin...

--- backend/src/main/java/com/leadestate/backend/repository/LeadStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leadestate.backend.repository;
-
-import com.leadestate.backend.entity.LeadStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadStatusRepository extends JpaRepository<LeadStatus, Integer> {
-}
--- backend/src/main/java/com/leadestate/backend/repository/RolePermissionRepository.java
+++
@@ -1,5 +0,0 @@
-package com.leadestate.backend.repository;
-
-public class RolePermissionRepository {
-    
-}
